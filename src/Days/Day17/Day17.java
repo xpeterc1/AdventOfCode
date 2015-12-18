e978c83427a982a1664c21238a4914730e3b719c
Peter Chow <xpeterc1@gmail.com>
2015-12-17 20:32:10 -0500

    Update file naming convention

--- src/Days/Day17/Day17.java
+++ src/Days/Day17/Day17.java
@@ -10,12 +10,11 @@ import java.util.Map;
 import AdventUtil.AdventFileReader;
 
 public class Day17 {
-	static int LITERS = 150;
+	static final int LITERS = 150;
 	static Map<Integer, Integer> combination_size = new HashMap<Integer, Integer>();
 	public static void main(String... args) throws IOException{
-		List<String> lines = AdventFileReader.getLines("Day17input.txt");
 		List<Integer> containers = new ArrayList<Integer>();
-		for(String line: lines){
+		for(String line: AdventFileReader.getLines("Day17input.txt")){
 			containers.add(Integer.parseInt(line));
 		}
 		System.out.println("Part 1: " + combinations(containers, 0, 0));
@@ -23,17 +22,12 @@ public class Day17 {
 		System.out.println("Part 2: " + combination_size.get(min_ContainerSize));
 	}
 	
-	//You can think of this solution as a binary tree, since there are 2^n combinations
 	public static int combinations(List<Integer> containers, int depth, int sum){
 		if(containers.isEmpty()){
 			return 0;
 		}
 		int value = containers.get(0);
-		
-		//left branch, include container (value + sum)
 		int include = combinations(containers.subList(1, containers.size()), depth + 1, value + sum);		
-		
-		//right branch, exclude container (0 + sum)
 		int exclude = combinations(containers.subList(1, containers.size()), depth, sum);				
 		if(sum + value == LITERS){
 			combination_size.put(depth, combination_size.containsKey(depth)? combination_size.get(depth) + 1 : 1);

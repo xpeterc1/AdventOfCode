66bba708cdc0d4a452322431059c6f3eef0917e1
Peter Chow <xpeterc1@gmail.com>
2015-12-21 05:14:20 -0500

    Final code cleanup to Day21

--- src/Days/Day21/Day21.java
+++ src/Days/Day21/Day21.java
@@ -3,106 +3,86 @@ package Days.Day21;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 //Leaderboard at 94th position, Yay
 public class Day21 {
+	static final int BOSS_HEALTH = 104;
+	static final int BOSS_DAMAGE = 8;
+	static final int BOSS_ARMOR = 1;
 	public static void main(String... args) throws IOException{
-		ArrayList<Integer> amountSpend = new ArrayList<Integer>();
-		ArrayList<Integer> amountSpendLOSSING = new ArrayList<Integer>();
-
-		for(Weapon weapon: Weapon.values()){
-			for(Armor armor: Armor.values()){
-				for(Ring ring1: Ring.values()){
-					for(Ring ring2: Ring.values()){
-						if(ring1 != ring2 || ring1 == Ring.NONE){
-							int cost = weapon.cost + armor.cost + ring1.cost + ring2.cost;							if(fightBoss(weapon,armor,ring1,ring2)){
-								amountSpend.add(cost);
+		ArrayList<Integer> totalCostWin = new ArrayList<Integer>();
+		ArrayList<Integer> totalCostLose = new ArrayList<Integer>();
+		for(Weapon weapon: Weapon.values()) 
+			for(Armor armor: Armor.values()) 
+				for(Ring ring1: Ring.values()) 
+					for(Ring ring2: Ring.values())
+						if(ring1.equals(Ring.NONE) || !ring1.equals(ring2)){
+							int cost = weapon.cost + armor.cost + ring1.cost + ring2.cost;							
+							if(fightBoss(weapon, armor, ring1, ring2)){
+								totalCostWin.add(cost);
 							}else{
-								amountSpendLOSSING.add(cost);
+								totalCostLose.add(cost);
 							}
 						}
-					}
-				}
-			}
-		}
-		System.out.println("Part 1: " + Collections.min(amountSpend));
-		System.out.println("Part 2: " + Collections.max(amountSpendLOSSING));
-
-
-
+		System.out.println("Part 1: " + Collections.min(totalCostWin));
+		System.out.println("Part 2: " + Collections.max(totalCostLose));
 	}
-	public static boolean fightBoss(Weapon weapon, Armor armor, Ring ring1, Ring ring2){
-		int bossHP = 104;
-		int bossDMG = 8;
-		int bossARMOR = 1;
+	private static boolean fightBoss(Weapon weapon, Armor armor, Ring ring1, Ring ring2){
+		final int heroDamage = weapon.damage + ring1.damage + ring2.damage;
+		final int damageToBoss = Math.max(1, heroDamage - BOSS_ARMOR);
+		final int heroArmor = armor.armor + ring1.armor + ring2.armor;
+		final int damageToHero = Math.max(1, BOSS_DAMAGE - heroArmor);
+		
+		int bossHP = BOSS_HEALTH;
 		int heroHP = 100;
-		int heroDMG = weapon.damage + ring1.damage + ring2.damage;
-		int heroARMOR = armor.armor + ring1.armor + ring2.armor;
+
 		while(true){
-			int damageToBoss = heroDMG - bossARMOR;
-			bossHP -= Math.max(1, damageToBoss);
-			if(bossHP <= 0){
-				return true;
-			}
-			int damageToHero = bossDMG - heroARMOR;
-			heroHP -= Math.max(1, damageToHero);
-			if(heroHP <= 0){
-				return false;
-			}
+			if((bossHP -= damageToBoss) <= 0) return true;
+			if((heroHP -= damageToHero) <= 0) return false;
 		}
 	}
 
 	static enum Weapon{
-		Dagger(8, 4, 0),
-		Shortsword(10, 5, 0),
-		Warhammer(25, 6, 0),
-		Longsword(40, 7, 0),
-		Greataxe(74, 8, 0);
+		Dagger		(8, 	4),
+		Shortsword	(10, 	5),
+		Warhammer	(25, 	6),
+		Longsword	(40, 	7),
+		Greataxe	(74, 	8);
 
-		private final int cost;
-		private final int damage;
-		private final int armor;
+		public final int cost, damage;
 
-		private Weapon(int cost, int damage, int armor){
+		private Weapon(int cost, int damage){
 			this.cost = cost;
 			this.damage = damage;
-			this.armor = armor;
-		}	
+		}
 	}
 
 	static enum Armor{
-		Leather(13, 0, 1),
-		Chainmail(31, 0, 2),
-		Splintermail(53, 0, 3),
-		Bandedmail(75, 0, 4),
-		Platemail(102, 0, 5),
-		NONE(0, 0, 0);
+		Leather		(13, 	1),
+		Chainmail	(31, 	2),
+		Splintermail(53, 	3),
+		Bandedmail	(75, 	4),
+		Platemail	(102, 	5),
+		NONE		(0, 	0);
 
-		private final int cost;
-		private final int damage;
-		private final int armor;
+		public final int cost, armor;
 
-		private Armor(int cost, int damage, int armor){
+		private Armor(int cost, int armor){
 			this.cost = cost;
-			this.damage = damage;
 			this.armor = armor;
 		}
 	}
 
 	static enum Ring{
-		Damage1(25, 1, 0),
-		Damage2(50, 2, 0),
-		Damage3(100, 3, 0),
-		Defense1(20, 0, 1),
-		Defense2(40, 0, 2),
-		Defense3( 80, 0, 3),
-		NONE( 0, 0, 0);
+		Damage1		(25, 	1, 		0),
+		Damage2		(50, 	2, 		0),
+		Damage3		(100, 	3, 		0),
+		Defense1	(20, 	0, 		1),
+		Defense2	(40, 	0, 		2),
+		Defense3	(80, 	0, 		3),
+		NONE		(0, 	0, 		0);
 
-		private final int cost;
-		private final int damage;
-		private final int armor;
+		public final int cost, damage, armor;
 
 		private Ring(int cost, int damage, int armor){
 			this.cost = cost;

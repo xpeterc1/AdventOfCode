6eb28c646d47c42ccd58cba58d7f57b155b56880
Peter Chow <xpeterc1@gmail.com>
2015-12-31 18:00:45 -0500

    Day 20, code cleanup

--- src/Days/Day20/Day20.java
+++ src/Days/Day20/Day20.java
@@ -1,62 +1,39 @@
 package Days.Day20;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Vector;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import AdventUtil.AdventFileReader;
 
 public class Day20 {
+	static final int INPUT = 36000000;
 	public static void main(String...args) throws IOException{
-		int target = 36000000;
-		System.out.println("Part 1: " + part1(target));
-		System.out.println("Part 2: " + part2(target));
-
+		System.out.println("Part 1: " + part1(INPUT));
+		System.out.println("Part 2: " + part2(INPUT));
 
 	}
 
 	public static int part1(int target){
-		int house = 831599;
-		int value = 0;
-		while(value <= target){
-			value = 0;
-			house++;
-			for(int elf = 1; elf <= house; elf++){
-				if(house%elf==0){
-					value += elf*10;
-					if(value == target){
-						break;
-					}
-				}
+		int[] houses = new int[1000000];
+		for(int i = 1; i < houses.length; i++) {
+			for(int j = i; j < houses.length; j+=i) {
+				houses[j] += i;
+			}
+			if(houses[i] * 10 >= target) {
+				return i;
 			}
 		}
-		return house;
+		return 0;
 	}
-
+	
 	public static int part2(int target){
-		int house = 884519;//862661
-		int value = 0;
-		int maxH = 0;
-		int maxV = 0;
-		while(value <= target){
-			value = 0;
-			house++;
-			for(int elf = (house/50)+1; elf <= house; elf++){
-				if(house%elf==0){
-					value += elf*11;
-					if(value == target){
-						break;
-					}
-				}
+		int[] houses = new int[1000000];
+		for(int i = 1; i < houses.length; i++) {
+			int fiftyHouses = Math.min(i * 50, houses.length);
+			for(int j = i; j < fiftyHouses; j+=i) {
+				houses[j] += i;
 			}
-			if(value > maxV){
-				maxV = value;
-				maxH = house;
+			if(houses[i] * 11 >= target) {
+				return i;
 			}
 		}
-		return house;
+		return 0;
 	}
 }
